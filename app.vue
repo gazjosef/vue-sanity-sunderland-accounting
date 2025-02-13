216b4c8071c73bf0cb18a681828ebad8bb4b56ce
Gareth Hind <garethjhind@gmail.com>
2025-02-13 18:58:28 +1100

    Refactor: Home Page

--- app.vue
+++ app.vue
@@ -1,6 +1,32 @@
+<script setup lang="ts">
+// import { ref, onMounted } from "vue";
+
+// const isDark = ref(false);
+
+// onMounted(() => {
+//   isDark.value = window.matchMedia("(prefers-color-scheme: dark)").matches;
+// });
+
+// const toggleDarkMode = () => {
+//   isDark.value = !isDark.value;
+//   document.documentElement.classList.toggle("dark", isDark.value);
+// };
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<style lang="scss">
+:root {
+  --bg-color: white;
+  --text-color: black;
+}
+.dark {
+  --bg-color: black;
+  --text-color: white;
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,10 @@
+<!-- <script setup lang="ts">
+const contactContent = await useSanity('*[_type == "contact"][0]');
+</script>
+
+<template>
+  <div class="contact">
+    <h1>{{ contactContent.title }}</h1>
+    <p>{{ contactContent.body }}</p>
+  </div>
+</template> -->
