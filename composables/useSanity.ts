0d90e46133054a38d759520bfaae027fdfc0eb5f
Gareth Hind <garethjhind@gmail.com>
2025-02-23 13:08:41 +1100

    Refactor: Rebuilt With Template

--- composables/useSanity.ts
+++
@@ -1,6 +0,0 @@
-// import { sanityClient } from "../sanity/sanity.cli";
-// import { sanityClient } from "@/sanity/client";
-
-// export const useSanity = async (query: string) => {
-//   return await sanityClient.fetch(query);
-// };
--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<!-- pages/index.vue -->
-<template>
-  <div>
-    <h1>Welcome to Home Page</h1>
-  </div>
-</template>
